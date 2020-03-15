c8c8d094a75f7586dab06a59622c4a21742c22f5
Holinhed <klatkiewiczdawid@gmail.com>
2020-03-15 00:26:57 -0400

    move along sin wave 2

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 
 // takes in one range of values in_min, in_max, and returns a range of out_min out_max
-const map = (num, in_min, in_max, out_min, out_max) => {
+function map(num, in_min, in_max, out_min, out_max) {
    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-let a = 0;
+var a = 0;
 misty.LinearVelocity(100);
 
-const doStuff = () => {
+function doStuff() {
 
    a += Math.PI / 10;
 
-   const s = sin(a);
-   const ms = map(s,-1,1,-100,100);
+   var s = Math.sin(a);
+   var ms = map(s,-1,1,-100,100);
 
    misty.AngularVelocity(ms);
 
